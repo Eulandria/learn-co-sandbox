7b3292c6e9322f8b3481bf1e25472afa3854046c
Eulandria Phillips <eulandria@mail.com>
2018-10-14 20:48:01 +0000

    Done.

---
+++ challenge.rb
@@ -0,0 +1,30 @@
+person = Arel
+def bag_maker(name)
+  "Hello,my name is #{name},"
+end
+
+def batch_badge_creator(array)
+  list_of_badges = []
+  array.each do |name| 
+    nuarry,push("Hello,my name is #{name}")
+end
+
+def assign_rooms(array)
+  nuarray = l
+  counter = 1
+  array.each do |name|
+   nuarry.push( Hello, #{name}! You'll be assigned to room #{counter}!"
+  counter += l
+  end
+  return nurray
+end
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    put id
+end
+assign rooms(array).each do |id|
+  puts id
+  end
+  
+end
+  
